736d140fff1c2caca682f524c098e2b8a91a45cd
HelgeEgil <helge.e.s.pettersen@gmail.com>
2024-11-22 14:12:49 +0100

    Working parser with inner / outer XML layer. Now I need a real example to work with

--- archetype_parser.py
+++ archetype_parser.py
@@ -1,89 +1,121 @@
 from lxml import etree
 import re
+from pprint import pprint
 
-def parse_contents_tree(xml_input):
-	tree = etree.parse(xml_input)
-	root = tree.getroot()
+def parse_outer_document(modified_wrapper):
+	root = etree.fromstring(modified_wrapper)
 
-	ns = {
+	ns0 = {
+		"i": "http://www.w3.org/2001/XMLSchema-instance",
+		"a": "http://schemas.datacontract.org/2004/07/Service.Dips_Rekvisisjon",
+		"x": "http://ihelse.net/wcf/2015/09"
+	}
+
+	req_list = root.xpath("//a:Rekvisisjon", namespaces=ns0)
+
+	keys = [
+		"avdeling",
+		"avd_reshid",
+		"dokumentformat",
+		"dokumenttype",
+		"dokumenttypeid",
+		"dokumentversjon",
+		"foedselsnr",
+		"foretak",
+		"forfatter",
+		"forfatterkode",
+		"forfatter_hprnr",
+		"forrigeid",
+		"godkjentav",
+		"godkjenttid",
+		"henvisningid",
+		"hfkortnavn",
+		"journalid",
+		"nprid",
+		"recordtype",
+		"setid",
+		"sistendrettid",
+		"skjema"
+	]
+
+	results = list()
+
+	for req in req_list:
+		req_dict = dict()
+		for key in keys:
+			res = req.xpath(f".//a:{key.upper()}/text()", namespaces=ns0)
+			res_parsed = len(res) and res[0] or None
+			req_dict[key] = res_parsed
+		results.append(req_dict)
+
+def parse_inner_document(inner_document):
+	def p(s):
+		return len(s) and s[0] or None
+
+	root = etree.fromstring(inner_document)
+
+	ns1 = {
 		"xsi": "http://www.w3.org/2001/XMLSchema-instance",
 		"p1": "http://www.w3.org/2001/XMLSchema-instance",
 		"x": "http://schemas.openehr.org/v1" # def. x her, er i default i filen
 	}
-	
-	# Find all contents
-	contents = root.xpath("//x:content", namespaces=ns)
-	print(f"found {len(contents)} items in contents")
+
+	contents = root.xpath("//x:content", namespaces=ns1)
+
+	results = list()
 
 	for content in contents:
-		name = content.xpath(".//x:name/x:value/text()", namespaces=ns)[0]
-		at = content.xpath(".//x:archetype_details/x:archetype_id/x:value/text()", namespaces=ns)[0]
-		print(f"Currently working with content = '{name}' using archetype '{at}'.")
-		cluster_items = content.xpath(".//x:items[@xsi:type='CLUSTER']", namespaces=ns)
+		name = p(content.xpath(".//x:name/x:value/text()", namespaces=ns1))
+		at = p(content.xpath(".//x:archetype_details/x:archetype_id/x:value/text()", namespaces=ns1))
 
+		content_dict = dict(
+			content_name=name,
+			archetype_name=at
+		)
+
+		element_items = content.xpath(".//x:items[@xsi:type='ELEMENT']", namespaces=ns1)
+		for element_item in element_items:
+			name = p(element_item.xpath(".//x:name/x:value/text()", namespaces=ns1))
+			value = p(element_item.xpath(".//x:value/x:value/text()", namespaces=ns1))
+
+			content_dict[name] = value
+		
+		cluster_items = content.xpath(".//x:items[@xsi:type='CLUSTER']", namespaces=ns1)
 		for cluster_item in cluster_items:
-			element_items = content.xpath(".//x:items[@xsi:type='ELEMENT']", namespaces=ns)
+			element_items = cluster_item.xpath(".//x:items[@xsi:type='ELEMENT']", namespaces=ns1)
 			for element_item in element_items:
-				name = element_item.xpath(".//x:name/x:value/text()", namespaces=ns)[0]
-				value = element_item.xpath(".//x:value/x:value/text()", namespaces=ns)[0]
-				print(f" * '{name}' = '{value}'") 
+				name = p(element_item.xpath(".//x:name/x:value/text()", namespaces=ns1))
+				value = p(element_item.xpath(".//x:value/x:value/text()", namespaces=ns1))
+				
+				content_dict[name] = value
 
-			cluster_name = cluster_item.xpath(".//x:name/x:value/text()", namespaces=ns)[0]
-			cluster_value = cluster_item.xpath(".//x:value/x:value/text()", namespaces=ns)[0]
-			print(f" ** '{cluster_name}' = '{cluster_value}' **")
+		results.append(content_dict)
 
-			element_items = cluster_item.xpath(".//x:items[@xsi:type='ELEMENT']", namespaces=ns)
-			for element_item in element_items:
-				name = element_item.xpath(".//x:name/x:value/text()", namespaces=ns)[0]
-				value = element_item.xpath(".//x:value/x:value/text()", namespaces=ns)[0]
-				print(f" *** '{name}' = '{value}'")
+	return results
 
-		print()
-
-def parse_archetype_tree(xml_input, input_file = False):
-	if input_file:
-		tree = etree.parse(xml_input)
-		root = tree.getroot()
-	else:
-		root = etree.fromstring(xml_input)
-
-	ns = {
-		"xsi": "http://www.w3.org/2001/XMLSchema-instance",
-		"p1": "http://www.w3.org/2001/XMLSchema-instance",
-		"x": "http://schemas.openehr.org/v1" # def. x her, er i default i filen
-	}
-
-	# Find previous RT treatment
-	at_list = root.xpath("//x:content", namespaces=ns)
-	for at in at_list:
-		archetype_node_id = at.xpath("@archetype_node_id")
-		if archetype_node_id[0] == "openEHR-EHR-OBSERVATION.procedure_screening.v1":
-			tidl_rt_list = at.xpath("//x:events//*[@archetype_node_id='at0022']/x:name/x:value/text()", namespaces=ns)
-			print(tidl_rt_list[0])
-
-	# Here at0022 refers to "Specific procedure"
-
-	# Find kartleggingsformal
-	x = root.xpath("//*[@archetype_node_id='at0034']/x:name/x:value/text()", namespaces=ns)[0]
-	y = root.xpath("//*[@archetype_node_id='at0034']/x:value/x:value/text()", namespaces=ns)[0]
-	z = root.xpath("//*[@archetype_node_id='at0034']/x:value/x:defining_code/x:code_string/text()", namespaces=ns)[0]
-	print(x, "er", y, "med SNOMED-CT kode", z)
-	
-def remove_dips_frame(xml_file):
-	with open(xml_file, "r") as f:
-		text = "".join(f.readlines())
+def split_document(xml_file):
+	with open(xml_file, "r", encoding="utf-8") as f:
+		wrapper_xml = f.read()
+		inner_documents = dict()
 
 		pattern = r"<a:SKJEMA[^>]*>(.*?)</a:SKJEMA>"
-		skjemas = re.findall(pattern, text, re.DOTALL)
 
-	return skjemas
+		def replace_with_placeholder(match):
+			index = len(inner_documents)
+			placeholder = f"SKJEMA_CONTENTS_{index}"
+			inner_documents[placeholder] = match.group(1).strip().replace(' encoding="utf-8"', "")
+			return f"<a:SKJEMA>{placeholder}</a:SKJEMA>"
 
-xml_file = "xml/rt_requisition_clean.xml"
-xml_string_list = remove_dips_frame(xml_file)
+		modified_wrapper = re.sub(pattern, replace_with_placeholder, wrapper_xml, flags=re.DOTALL)
+		
+		return modified_wrapper, inner_documents
 
-"""
-for xml_string in xml_string_list:
-	result = parse_archetype_tree(xml_string.encode("utf-8"))
-"""
 
-parse_contents_tree(xml_file)
+xml_file = "xml/rt_requisition.xml"
+
+modified_wrapper, inner_documents = split_document(xml_file)
+parse_outer_document(modified_wrapper)
+
+parsed_inner_document = dict()
+for name, inner_document in inner_documents.items():
+	parsed_inner_document[name] = parse_inner_document(inner_document)
